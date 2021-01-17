311127f610d1f25b4c85cd53fe00e936dcf600d3
lexusflexus <74801170+lexusflexus@users.noreply.github.com>
2021-01-17 16:18:44 -0600

    Add files via upload

---
+++ bind.sh
@@ -0,0 +1,54 @@
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p udp --dport 53-j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -P INPUT DROP
+iptables -N LOG_DROP
+iptables -A LOG_DROP -j LOG --log-prefix "OUTPUT:DROP" --log-level 6
+iptables -A OUTPUT  -d 10.0.1.70 -j  LOG_DROP
+iptables -A LOG_DROP -j DROP
+
+
+chsh -s /bin/false daemon
+chsh -s /bin/false bin
+chsh -s /bin/false sys
+chsh -s /bin/false games
+chsh -s /bin/false man
+chsh -s /bin/false news
+chsh -s /bin/false lp
+chsh -s /bin/false news
+chsh -s /bin/false uucp
+chsh -s /bin/false proxy
+chsh -s /bin/false www-data
+chsh -s /bin/false backup
+chsh -s /bin/false list
+chsh -s /bin/false irc
+chsh -s /bin/false gnats
+chsh -s /bin/false nboody
+chsh -s /bin/false libuuid
+
+
+service nscd stop
+service proftd stop
+service cron stop
+service ssh stop
+service atd stop
+
+apt-get purge cron
+
+cd/home/sysadmin
+rm -rf /home/*
+mkdir /home/sysadmin
+
+rm -rf /usr/local/sbin/*
+
+cd ~
+
+rm .ssh 
+rm .s
+rm .rdn
+
+
+touch logon.txt
+cat > "This system is for the use of authorized users only."
+mv logon.txt /etc/
